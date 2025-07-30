8ac7d2f63478aab4a5bc878f3cde0ae3a3671fae
crunchydeer30 <crunchydeer30@gmail.com>
2025-07-30 22:38:23 +0300

    refactor insertion sort list

--- 12-insertion-sort/insertion_sort_list.go
+++ 12-insertion-sort/insertion_sort_list.go
@@ -1,84 +1,38 @@
 package main
 
-import "fmt"
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
-func main() {
-	head := createLinkedList([]int{1, 2, 0, -1, 5, 88, -9})
-	printLinkedList(head)
-	head = insertionSortList(head)
-	printLinkedList(head)
-}
-
 func insertionSortList(head *ListNode) *ListNode {
-	curr := head.Next
-	prev := head
-
-	for curr != nil {
-		if curr.Val < prev.Val {
-			node := curr
-			curr = curr.Next
-			prev.Next = curr
-
-			if node.Val <= head.Val {
-				node.Next = head
-				head = node
-				continue
-			}
-
-			currInner := head.Next
-			prevInner := head
-
-			for currInner != curr {
-				if node.Val <= currInner.Val {
-					node.Next = currInner
-					prevInner.Next = node
-					break
-				}
-				prevInner = currInner
-				currInner = currInner.Next
-			}
-
-		} else {
-			prev = curr
-			curr = curr.Next
-		}
+	if head == nil || head.Next == nil {
+		return head
 	}
 
-	return head
-}
+	dummy := &ListNode{Next: head}
+	sortedTail := head
+	curr := head.Next
 
-func createLinkedList(arr []int) *ListNode {
-	var head *ListNode = nil
-	var curr *ListNode = head
-
-	for _, val := range arr {
-		if head == nil {
-			node := &ListNode{Val: val, Next: nil}
-			head = node
-			curr = head
+	for curr != nil {
+		if curr.Val >= sortedTail.Val {
+			sortedTail, curr = curr, curr.Next
 			continue
 		}
 
-		node := &ListNode{
-			Val:  val,
-			Next: nil,
+		sortedTail.Next = curr.Next
+		node := curr
+		curr = sortedTail.Next
+
+		prev := dummy
+
+		for prev.Next.Val < node.Val {
+			prev = prev.Next
 		}
-		curr.Next = node
-		curr = curr.Next
+
+		node.Next = prev.Next
+		prev.Next = node
 	}
 
-	return head
-}
-
-func printLinkedList(head *ListNode) {
-	for head != nil {
-		fmt.Print(head.Val, " ")
-		head = head.Next
-	}
-	fmt.Println()
+	return dummy.Next
 }
