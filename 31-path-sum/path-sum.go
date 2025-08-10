bdfdcc2466b07d0a26519b64e55396f38638f431
crunchydeer30 <crunchydeer30@gmail.com>
2025-08-10 21:29:13 +0300

    add task

---
+++ 31-path-sum/path-sum.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+		Right: &TreeNode{
+			Val: 3,
+		},
+	}
+
+	fmt.Println(hasPathSum(root, 3))
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	targetSum -= root.Val
+
+	if root.Left == nil && root.Right == nil {
+		return targetSum == 0
+	}
+
+	return hasPathSum(root.Left, targetSum) || hasPathSum(root.Right, targetSum)
+}
